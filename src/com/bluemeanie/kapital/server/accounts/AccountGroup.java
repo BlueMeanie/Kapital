7bbd06dcfecd8126edc173d0281370c5f89ef63f
BlueMeanie <josh.josh421@gmail.com>
2012-03-01 16:51:27 -0700

    adding AccountGroup placeholder

---
+++ src/com/bluemeanie/kapital/server/accounts/AccountGroup.java
@@ -0,0 +1,13 @@
+package com.bluemeanie.kapital.server.accounts;
+
+public class AccountGroup {
+
+	/**
+	 * @param args
+	 */
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+	}
+
+}
