c9d8e8bb8b9b62863b7143d3661a8d332fff7b04
BlueMeanie <josh.josh421@gmail.com>
2012-03-01 14:20:01 -0700

    added Contract class

---
+++ src/com/bluemeanie/kapital/shared/core/Contract.java
@@ -0,0 +1,91 @@
+package com.bluemeanie.kapital.shared.core;
+
+import java.io.IOException;
+import java.io.Serializable;
+import java.security.InvalidKeyException;
+import java.security.KeyPair;
+import java.security.KeyPairGenerator;
+import java.security.NoSuchAlgorithmException;
+import java.security.PrivateKey;
+import java.security.Provider;
+import java.security.PublicKey;
+import java.security.Security;
+import java.security.Signature;
+import java.security.SignatureException;
+
+public class Contract extends Message
+ implements Serializable {
+
+	byte[] signatureRecipient;
+	byte[] signatureNotary;
+	
+	Contract(){};
+	
+	Contract( String payload ){
+		super(payload);
+	}
+	
+	Contract( String payload, PrivateKey signer ){};
+	
+	byte[] signRecipient( PrivateKey privateKey , Signature sig ) throws InvalidKeyException, SignatureException{
+		sig.initSign( privateKey );
+	    sig.update( payload );
+	    this.signatureRecipient = sig.sign();
+	    return this.signatureRecipient;
+	}
+	
+	byte[] signNotary( PrivateKey privateKey , Signature sig ) throws InvalidKeyException, SignatureException{
+		sig.initSign( privateKey );
+	    sig.update( payload );
+	    this.signatureNotary = sig.sign();
+	    return this.signatureNotary;
+	}
+	
+	
+	
+	/**
+	 * @param args
+	 * @throws NoSuchAlgorithmException 
+	 * @throws InvalidKeyException 
+	 * @throws SignatureException 
+	 */
+	public static void main(String[] args) throws NoSuchAlgorithmException, InvalidKeyException, SignatureException {
+
+		Contract testContract = new Contract("E PLURIBUS UNUM.");
+
+	    //
+	    // generate an RSA keypair
+	    System.out.println( "\nStart generating RSA key" );
+	    KeyPairGenerator keyGen = KeyPairGenerator.getInstance("RSA");
+	    keyGen.initialize(1024);
+	    
+	    KeyPair intiatorKey = keyGen.generateKeyPair();
+	    KeyPair recipientKey = keyGen.generateKeyPair();
+	    KeyPair notaryKey = keyGen.generateKeyPair();
+
+	    System.out.println( "Finish generating RSA keys" );
+	    //
+	    // get a signature object using the MD5 and RSA combo
+	    // and sign the plaintext with the private key,
+	    // listing the provider along the way
+	    
+	    Signature sig = Signature.getInstance("MD5WithRSA");
+	    // creates the cipher
+	    
+	    // sign all three
+	    testContract.signInitiator(intiatorKey.getPrivate(), sig);
+	    testContract.signRecipient(recipientKey.getPrivate(), sig);
+	    testContract.signNotary(notaryKey.getPrivate(), sig);
+	    
+	    try {
+		      if ( testContract.verify(intiatorKey.getPublic(), sig) ) {
+		        System.out.println( "Initiator Signature verified" );
+		      } else System.out.println( "Signature failed" );
+		    } catch (SignatureException se) {
+		      System.out.println( "Signature failed" );
+		    }
+	    
+
+	}
+
+}
